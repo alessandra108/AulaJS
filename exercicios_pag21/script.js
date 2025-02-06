d3f9321623deb76de412945a10004ceda5691386
alessandra108 <alessandra10876@gmail.com>
2025-02-06 17:07:25 -0300

    Add files via upload

---
+++ exercicios_pag21/script.js
@@ -0,0 +1,99 @@
+// ------exercicio 1------
+
+// let notaAluno =  parseInt(prompt("digite sua nota"));
+
+// if(notaAluno >= 7 && notaAluno <= 10){
+//     console.log("Aprovado");
+// }
+
+// else if(notaAluno >= 5 && notaAluno < 7){
+// console.log("Recuperação");
+// }
+
+// else if(notaAluno < 5){
+//     console.log("Reprovado");
+// }
+
+// else{
+//     console.log("Nota invalida");
+// }
+
+
+//-----exercicio 2-----
+
+// let Mes = parseInt(prompt("digite o número do mes"));
+// switch(Mes){
+    
+//     case 1: 
+//     msg = "Janeiro";
+//     break;
+
+//     case 2: 
+//     msg = "Fevereiro";
+//     break;
+
+//     case 3: 
+//     msg = "Março";
+//     break;
+
+//     case 4: 
+//     msg = "Abril";
+//     break;
+
+//     case 5: 
+//     msg = "Maio";
+//     break;
+
+//     case 6: 
+//     msg = "Junho";
+//     break;
+
+//     case 7: 
+//     msg = "Julho";
+//     break;
+
+//     case 8: 
+//     msg = "Agosto";
+//     break;
+
+//     case 9: 
+//     msg = "Setembro";
+//     break;
+
+//     case 10: 
+//     msg = "Outubro";
+//     break;
+
+//     case 11: 
+//     msg = "Novembro";
+//     break;
+
+//     case 12: 
+//     msg = "Dezembro";
+//     break;
+
+//     default:
+//     msg = "Mes invalido";
+    
+// }
+// console.log(msg);
+
+//-----exercicio 3-----
+
+// let acesso = prompt("Digite o seu acesso: adm - usuario - visitante");
+
+// if(acesso === "adm"){
+//     console.log("Acesso Permitido");
+// }
+
+// else if(acesso === "usuario" ){
+//     console.log("Acesso permitido");
+// }
+
+// else if(acesso === "visitante"){
+//     console.log("Acesso permitido");
+// }
+
+// else{
+//     console.log("Acesso negado, por favor logue na sua conta");
+// }
